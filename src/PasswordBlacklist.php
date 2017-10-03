9b96284b1c5dbd184d08536bfdc5876dbeb84a27
Reedy <reedy@wikimedia.org>
2017-10-03 01:39:45 +0100

    Initial implementation of the PasswordBlacklist
    Implemented using a Bloom Filter from pleonasm/bloom-filter [1]
    
    Password list source came from GitHub [2]
    
    [1] https://github.com/pleonasm/bloom-filter
    [2] https://github.com/danielmiessler/SecLists/blob/aad07ff/Passwords/10_million_password_list_top_100000.txt
    
    Change-Id: I2a8f8bd4fd73982e7ef6921ac93ae6e3e87bcfa1

--- src/PasswordBlacklist.php
+++ src/PasswordBlacklist.php
@@ -1,7 +1,29 @@
 <?php
 
-namespace Wikimedia;
+namespace Wikimedia\PasswordBlacklist;
+
+use Pleo\BloomFilter\BloomFilter;
 
 class PasswordBlacklist {
 
+	/**
+	 * @return BloomFilter
+	 */
+	private static function getFilter() {
+		static $filter = null;
+		if ( $filter === null ) {
+			$filter = BloomFilter::initFromJson(
+				json_decode( file_get_contents( __DIR__ . '/blacklist.json' ), true )
+			);
+		}
+		return $filter;
+	}
+
+	/**
+	 * @param string $password Password to check if in the Bloom Filter
+	 * @return bool
+	 */
+	public static function isBlacklisted( $password ) {
+		return self::getFilter()->exists( $password );
+	}
 }
--- tests/PasswordBlacklistTest.php
+++ tests/PasswordBlacklistTest.php
@@ -2,6 +2,55 @@
 
 namespace Wikimedia\PasswordBlacklist\Test;
 
-class PasswordBlacklistTest {
+use Wikimedia\PasswordBlacklist\PasswordBlacklist;
 
+class PasswordBlacklistTest extends \PHPUnit_Framework_TestCase {
+
+	public static function getBlacklistedPasswords() {
+		$lines = [];
+		$file = fopen(
+			dirname( __DIR__ ) . '/scripts/data/10_million_password_list_top_100000.txt',
+			'r'
+		);
+		while ( !feof( $file ) ) {
+			$line = trim( fgets( $file ) );
+			if ( !$line ) {
+				continue;
+			}
+			$lines[] = [ $line ];
+		}
+
+		return $lines;
+	}
+
+	/**
+	 * Tests that all the passwords in the original 10_million_password_list_top_100000.txt file
+	 * are in the BloomFilter.
+	 *
+	 * @dataProvider getBlacklistedPasswords
+	 * @covers \Wikimedia\PasswordBlacklist\PasswordBlacklist::isBlacklisted
+	 * @param string $password
+	 */
+	function testBlacklistedPassword( $password ) {
+		$this->assertTrue( PasswordBlacklist::isBlacklisted( $password ) );
+	}
+
+	public static function getNonBlacklistedPasswords() {
+		return [
+			[ 'MediaWiki' ],
+			[ 'Wikipedia' ],
+			[ 'Wikimedia' ],
+		];
+	}
+
+	/**
+	 * Tests a few passwords that aren't in the blacklist
+	 *
+	 * @dataProvider getNonBlacklistedPasswords
+	 * @covers \Wikimedia\PasswordBlacklist\PasswordBlacklist::isBlacklisted
+	 * @param string $password
+	 */
+	function testNonBlacklistedPasswords( $password ) {
+		$this->assertFalse( PasswordBlacklist::isBlacklisted( $password ) );
+	}
 }
