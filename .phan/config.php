4796480caaa89329ae54d15a33f6c43b9691e3db
Reedy <reedy@wikimedia.org>
2022-03-15 01:34:01 +0000

    Add phan
    Bug: T303786
    Change-Id: Ie1d04eb25f4b7415a47a4586364fca0ac065821b

---
+++ .phan/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['directory_list'] = [
+	'vendor/pleonasm/bloom-filter/src',
+];
+
+$cfg['exclude_analysis_directory_list'] = [
+	'vendor/pleonasm/bloom-filter/src',
+];
+
+return $cfg;
