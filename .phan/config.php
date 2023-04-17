bd3d29033f2e686aa21a0d649cc75377b4f82b24
James D. Forrester <jforrester@wikimedia.org>
2023-04-17 16:27:46 -0400

    build: Switch phan to special library mode
    Bug: T334492
    Change-Id: Ifc6b7dfab0bff732311265e8df3c31ccf72eecac

--- .phan/config.php
+++ .phan/config.php
@@ -1,5 +1,5 @@
 <?php
 
-$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
 
 return $cfg;
