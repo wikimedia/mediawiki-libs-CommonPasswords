cc2f8d067675e0d1eb7614d604169a6c76fd6cac
Reedy <reedy@wikimedia.org>
2025-05-21 21:23:25 +0000

    CI: Exclude common.php from phan
    As long as it's valid php (and php-parallel-lint seems ok with it),
    we don't really care what phan thinks!
    
    Bug: T394821
    Change-Id: I667920b7dd134c888d929d7f8eff9844fa962cdf

--- .phan/config.php
+++ .phan/config.php
@@ -2,4 +2,12 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
 
+$cfg['exclude_file_list'] = array_merge(
+	$cfg['exclude_file_list'],
+	[
+		// T394821
+		'src/common.php',
+	]
+);
+
 return $cfg;
